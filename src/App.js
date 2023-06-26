07aa0a78360b3d880a79ee77d11417a9cf1a96b4
manoj <manojlakshan9991@gmail.com>
2023-06-26 00:01:59 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-//import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
 import FeedbackList from './component/FeedbackList';
